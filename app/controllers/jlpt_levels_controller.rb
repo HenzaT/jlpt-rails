e9bd7095dfd69921b059a12bba7d2b184c3f7049
HenzaT <henrykun95@gmail.com>
2026-08-08 10:58:16 +0100

    took out unused variable

--- app/controllers/jlpt_levels_controller.rb
+++ app/controllers/jlpt_levels_controller.rb
@@ -3,7 +3,7 @@ class JlptLevelsController < ApplicationController
   def index
     @levels = [5, 4, 3, 2, 1]
     @kanji_character_counts = KanjiCharacter.group(:jlpt).count
-    @learnt_kanji = current_user.learnt_kanjis
+    # @learnt_kanji = current_user.learnt_kanjis
     all_kanjis = KanjiCharacter.all
     # instance variable for show random kanji button
     @random_kanji = all_kanjis.sample
